ced50964941efa6f2d1a209d6793316498841fd3
Dimas Setyawan Ramadhansyah <dimasrama.business@gmail.com>
2023-11-29 11:16:25 +0700

    Update file

--- function_as_parameter.go
+++ function_as_parameter.go
@@ -1,9 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var data = []string{"Dimas", "Setyawan", "Ramadhansyah"}
+	var dataContains = filter(data, func(v string) bool {
+		return strings.Contains(v, "h")
+	})
+
+	var dataLength = filter(data, func(v string) bool {
+		return len(v) == 5
+	})
+	fmt.Println(dataContains)
+	fmt.Println(dataLength)
+}
+
+type Filter func(string) bool
+
+func filter(data []string, callback Filter) []string {
+	var result []string
+	for _, v := range data {
+		if callback(v) {
+			result = append(result, v)
+		}
+	}
+	return result
 }
 
 /*
 * Fungsi bisa dijadikan tipe data variable, Sehingga bisa dijadikan parameter
+* TEMUAN
+*	Bisa digunakan untuk lebih dari satu func dengan tipe data sama
 * */
